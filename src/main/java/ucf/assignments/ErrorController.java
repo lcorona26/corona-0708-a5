5fe66f6f86d9eb92172124ea3308fd113a7201d1
lcorona26 <luiscorona2602@gmail.com>
2021-07-24 15:10:44 -0400

    Added ErrorController for displaying error messages in add function

---
+++ src/main/java/ucf/assignments/ErrorController.java
@@ -0,0 +1,28 @@
+package ucf.assignments;
+
+import javafx.scene.control.Alert;
+
+public class ErrorController {
+
+    public void name(){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText("Incorrect Name Format!");
+        alert.setContentText("Name must be between 2 and 252 characters");
+        alert.showAndWait();
+
+    }
+    public void serial(){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText("Incorrect Serial Number Format!");
+        alert.setContentText("Serial Number must be at least and no more 10 characters");
+        alert.showAndWait();
+
+    }
+    public void value(){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText("Incorrect Value Format!");
+        alert.setContentText("Value must be a number");
+        alert.showAndWait();
+    }
+
+}
